8a0b8e6baa8634ce25beef144ea30769322ff032
JOAO VICTOR DINIZ PIAZZA <144743654+JoaoPiazza07@users.noreply.github.com>
2024-09-26 14:53:31 -0300

    Add files via upload

---
+++ app-expo/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import HomePage from './src/Pages/HomePage';
+
+const App = () => {  //função homepage
+  return( //conteudo jsx (html)
+
+    <HomePage /> //app principal esta enderizando o home page
+    
+  );
+
+} 
+
+export default App; //expoprta essa função
---
+++ app-expo/src/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {  //função homepage
+  return( //conteudo jsx (html)
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+
+      <Text> Puc Minas </Text>
+      
+
+    </View> //funciona como uma div, como conteiner
+
+  );
+
+} 
+
+export default HomePage; //expoprta essa função
